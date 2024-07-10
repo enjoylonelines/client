9187a6bef10617aab6b43f6cdadef940317cb637
wooka000 <wooka000@naver.com>
2024-07-11 00:44:31 +0900

    Feat : 지도 연동 및 마커 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,6 +9,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          strategy="afterInteractive"
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_CLIENT_ID}`}
+        ></Script>
         <div className="flex flex-col items-center bg-slate-300">
           <div className="max-w-[430px] min-h-screen w-full h-full bg-white">
             {children}
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,22 +2,16 @@ import React from "react";
 import { GoBell } from "react-icons/go";
 import { TfiWrite } from "react-icons/tfi";
 import { IoMenu } from "react-icons/io5";
+import Image from "next/image";
 
+{
+  /* <GoBell className="w-5 h-5" />
+<IoMenu className="w-5 h-5" /> */
+}
 export default function Header() {
   return (
-    <div className="flex justify-between">
-      <div className="w-3/4">logo</div>
-      <div className="flex items-center w-1/4">
-        <div className="w-full">
-          <GoBell className="w-5 h-5" />
-        </div>
-        <div className="w-full">
-          <TfiWrite className="w-5 h-5" />
-        </div>
-        <div className="w-full">
-          <IoMenu className="w-5 h-5" />
-        </div>
-      </div>
+    <div className="flex items-center justify-center my-3">
+      <Image src="/mapstory_logo.png" alt="logo" width={230} height={54} />
     </div>
   );
 }
